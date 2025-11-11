7c4f530d67cfd602037788695a8f091653519ab4
ema munoz <munozema81@gmail.com>
2025-11-11 12:15:38 -0600

    Commit con la organizacion de las carpetas en el repositorio y primeros metodos en pseudocodigo.

---
+++ src/com/ucr/sheets/Main.java
@@ -0,0 +1,7 @@
+package com.ucr.sheets;
+
+public class Main {
+  public static void main (final String [] args) {
+    System.out.println("Hello word!");
+  }
+}
---
+++ src/com/ucr/sheets/package.info.java
@@ -0,0 +1,10 @@
+/**
+ * Paquete con la solución al 2do proyecto del curso CI0121-2025 || Programación I.
+ * @author Ema Muñoz
+ * @author Andrés Zúñiga
+ * @version 1.0
+ */
+
+package com.ucr.sheets;
+
+
